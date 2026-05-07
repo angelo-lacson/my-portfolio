afaf30880bd7af8dd5bc69c19871b33d3b4946b8
Angelo Lacson <angelo.lacson.work0407@gmail.com>
2026-05-08 04:22:16 +0800

    🔗 feat: add company website links to experience cards

--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -1,13 +1,20 @@
 // @flow strict
-
+"use client";
 import { experiences } from "@/utils/data/experience";
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import experience from '../../../assets/lottie/code.json';
 import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
+import { useRouter } from "next/navigation";
 
 function Experience() {
+  const router = useRouter();
+  const gotoCompanyWebsite = (website) => {
+    if (website) {
+      router.push(website);
+    }
+  };
   return (
     <div id="experience" className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
       <Image
@@ -40,6 +47,7 @@ function Experience() {
             <div className="flex flex-col gap-6">
               {
                 experiences.map(experience => (
+                  <div key={experience.id} onClick={() => gotoCompanyWebsite(experience.website)}>
                   <GlowCard key={experience.id} identifier={`experience-${experience.id}`}>
                     <div className="p-3 relative">
                       <Image
@@ -69,6 +77,7 @@ function Experience() {
                       </div>
                     </div>
                   </GlowCard>
+                  </div>
                 ))
               }
             </div>
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,24 +3,28 @@ export const experiences = [
     id: 1,
     title: 'Senior Platform Engineer',
     company: "Tecton.ai",
-    duration: "(Aug 2023 - Jan 2026)"
+    duration: "(Aug 2023 - Jan 2026)",
+    website: "https://tecton.ai/"
   },
   {
     id: 2,
     title: "Senior Backend & DevOps Engineer",
     company: "Gatekeeper",
-    duration: "(Jun 2021 - Jul 2023)"
+    duration: "(Jun 2021 - Jul 2023)",
+    website: "https://www.gatekeeperhq.com/"
   },
   {
     id: 3,
     title: "Software Engineer",
     company: "Dreamix",
-    duration: "(Nov 2019 - Apr 2021)"
+    duration: "(Nov 2019 - Apr 2021)",
+    website: "https://dreamix.eu/"
   },
   {
     id: 4,
     title: "Full-Stack & Platform Engineer",
     company: "Arcanys",
-    duration: "(Sep 2018 - Sep 2019)"
+    duration: "(Sep 2018 - Sep 2019)",
+    website: "https://www.arcanys.com/"
   }
 ]
