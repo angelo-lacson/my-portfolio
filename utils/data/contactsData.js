d4e64db13fd298bbe580a403c78694d85b5d7121
Angelo Lacson <angelo.lacson.work0407@gmail.com>
2026-05-08 22:26:07 +0800

    🐛 fix: fix typo errors

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'angelo.lacson.work0407@gmail.com',
     phone: '+639690513850',
-    address: 'Tizon Street, Poblacion, Santo Tomas, Pampanga, PHLIPPINES',
+    address: 'Tizon Street, Poblacion, Santo Tomas, Pampanga, PH',
     github: 'https://github.com/angelo-lacson',
     facebook: 'https://www.facebook.com/angelo.lacson0407/',
     linkedIn: 'https://www.linkedin.com/in/angelo-lacson-81808b3b6/',
